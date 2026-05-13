9d7db068c7096cd2cb1bf2613346779659577145
sabbysharetime-png <sabbysharetime@gmail.com>
2026-05-13 14:45:44 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'sabby-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.jpg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
